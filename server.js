81d9227e14fa5469a243defeec19641ef064d15d
Arvin Bayat Manesh <aba191@sfu.ca>
2023-12-27 15:40:50 -0800

    Added the back button on the Sign Up page.

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+// Middleware to set Content-Security-Policy header
+app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', 'default-src \'self\''); // Update this policy as needed
+    next();
+});
+
 // Define a route to serve the "index.html" file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/pages/index.html');
+});
+
+// Redirect requests for "/index.html" to the root path "/"
+app.get('/index.html', (req, res) => {
+    res.redirect('/');
 });
 
 // Define a route to serve the "sign_up.html" file
