77b9fcb6fca9117f6a6b28c61883f322630e675d
Arvin Bayat Manesh <aba191@sfu.ca>
2024-01-06 15:42:52 -0800

    Validation in book recommendation form is complete. Bugs are fixed.

--- public/scripts/book_song_recommendation.js
+++ public/scripts/book_song_recommendation.js
@@ -1,4 +1,4 @@
-function handleInputValidity() {
+function handleInputValiditySong() {
     const songName = document.querySelector("#song-name");
     const artistName = document.querySelector("#artist-name");
     const songNameErrorMessage = document.querySelector("#song-name-error-message");
@@ -15,6 +15,9 @@ function handleInputValidity() {
     } else {
         artistNameErrorMessage.textContent = "";
     }
+
+    // If any of the required fields are empty return false.
+    return !(songName.value === "" || artistName.value === "");
 }
 
 function handleRecommendedSong() {
@@ -24,6 +27,7 @@ function handleRecommendedSong() {
     const songDescription = document.querySelector("#song-description");
     const songRecommendButton = document.querySelector("#song_recommend");
     const songRecommendForm = document.querySelector("#song_form");
+    let validatedSuccessfully = true;
 
     // Set an event listener for song recommendation button.
     songRecommendForm.addEventListener("submit", function(event) {
@@ -36,21 +40,71 @@ function handleRecommendedSong() {
         event.preventDefault();
 
         // Validate the inputs.
-        handleInputValidity();
-        console.log("WE GOT HEREE");
+        validatedSuccessfully = handleInputValiditySong();
+
+        if (validatedSuccessfully) {
+            // TODO
+        }
     }
 }
 
+function handleInputValidityBook() {
+    const bookName = document.querySelector("#book-name");
+    const authorName = document.querySelector("#author-name");
+    const bookNameErrorMessage = document.querySelector("#book-name-error-message");
+    const authorNameErrorMessage = document.querySelector("#book-author-error-message");
+
+    if (bookName.value === "") {
+        bookNameErrorMessage.textContent = "Book name is required.";
+    } else {
+        bookNameErrorMessage.textContent = "";
+    }
+
+    if (authorName.value === "") {
+        authorNameErrorMessage.textContent = "Author name is required.";
+    } else {
+        authorNameErrorMessage.textContent = "";
+    }
+
+    // If any of the required fields are empty return false.
+    return !(bookName.value === "" || authorName.value === "");
+}
+
 function handleRecommendedBook() {
-    // TODO
+    const bookName = document.querySelector("#book-name");
+    const authorName = document.querySelector("#author-name");
+    const publicationYear = document.querySelector("#publication-year");
+    const bookDescription = document.querySelector("#book-description");
+    const bookRecommendButton = document.querySelector("#book_recommend");
+    const bookRecommendForm = document.querySelector("#book_form");
+    let validatedSuccessfully = true;
+
+    // Set an event listener for book recommendation button.
+    bookRecommendForm.addEventListener("click", function(event) {
+        event.preventDefault();
+    });
+
+    bookRecommendButton.addEventListener("click", handleRecommendClick);
+
+    function handleRecommendClick(event) {
+        event.preventDefault();
+
+        // Validate the inputs.
+        validatedSuccessfully = handleInputValidityBook();
+
+        if (validatedSuccessfully) {
+            // TODO
+        }
+    }
+
 }
 
 window.onload = function() {
     // Determine if the user clicked on the book or song recommendation.
     const urlParams = new URLSearchParams(window.location.search);
-    const isSongRecommendation = urlParams.get('isSongRecommendation');
+    const isSongRecommendationStr = urlParams.get('isSongRecommendation');
 
-    if (isSongRecommendation) {
+    if (isSongRecommendationStr === "true") {
         // Add the new recommended song.
         handleRecommendedSong();
     }
