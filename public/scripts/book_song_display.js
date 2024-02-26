d0e452a34f2f658dadcc6db206664ec9f5153021
Arvin Bayat Manesh <aba191@sfu.ca>
2024-02-25 23:27:16 -0800

    Added the dynamic table functionality to the song_display.html

--- public/scripts/book_song_display.js
+++ public/scripts/book_song_display.js
@@ -2,7 +2,11 @@ import {SongController} from "./song_controller.js";
 import {BookController} from "./book_controller.js";
 
 function deleteTableRows(tableToBeDeleted) {
-    // TODO
+    if (tableToBeDeleted.rows.length > 0) {
+        while (tableToBeDeleted.rows.length > 0) {
+            tableToBeDeleted.deleteRow(0);
+        }
+    }
 }
 
 export default function displaySongsBooks(clickedCountry) {
@@ -27,13 +31,53 @@ export default function displaySongsBooks(clickedCountry) {
         // Populate the table with the songs corresponding to the country that was clicked.
         for (const song of songList) {
             if (song._recommending_user_country.toLowerCase() === clickedCountry.toLowerCase()) {
-                console.log("Going to populate the table with the song");
+                // Create a new row in the table.
+                const newRow = document.createElement("tr");
+
+                const songNameCell = document.createElement("td");
+                let cellContent = `Song Name: ${song._song_name} <br> 
+                      Artist Name: ${song._artist} <br> 
+                      Recommended By: ${song._recommending_user} <br> 
+                      Country Of The Recommending User: ${song._recommending_user_country}`;
+
+                // Add the year of release and the description if the user provided them.
+                if (song._release_year !== null) {
+                    cellContent += `<br> Release Year: ${song._release_year}`;
+                }
+
+                if (song._description !== null) {
+                    cellContent += `<br> Description: ${song._description}`;
+                }
+
+                songNameCell.innerHTML = cellContent;
+                newRow.appendChild(songNameCell);
+                songTable.appendChild(newRow);
             }
 
             // The case where the country clicked was United States.
             if (song._recommending_user_country.toLowerCase() === "usa"
                 && clickedCountry.toLowerCase() === "united states of america") {
-                console.log("Going to recommend US song!");
+                // Create a new row in the table.
+                const newRow = document.createElement("tr");
+
+                const songNameCell = document.createElement("td");
+                let cellContent = `Song Name: ${song._song_name} <br> 
+                      Artist Name: ${song._artist} <br> 
+                      Recommended By: ${song._recommending_user} <br> 
+                      Country Of The Recommending User: ${song._recommending_user_country}`;
+
+                // Add the year of release and the description if the user provided them.
+                if (song._release_year !== null) {
+                    cellContent += `<br> Release Year: ${song._release_year}`;
+                }
+
+                if (song._description !== null) {
+                    cellContent += `<br> Description: ${song._description}`;
+                }
+
+                songNameCell.innerHTML = cellContent;
+                newRow.appendChild(songNameCell);
+                songTable.appendChild(newRow);
             }
         }
     }
